61afc4b22c4d17cbc54bd575eb7b5a04552c8a75
chrizxzayax <chmolina629@hotmail.com>
2025-10-30 19:54:53 -0700

    add the main for main loop

--- lab26.cpp
+++ lab26.cpp
@@ -1,1 +1,30 @@
 // comsc 210 | lab26 | Christian Molina
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <list>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+const int NUM_RUNS = 15;// number of runs for averaging
+const int STRUCTURES = 3;// vector, list, set
+const int ROWS = 4;// for data sizes
+const int COLS = STRUCTURES;// for vector, list, set
+
+
+
+int main() {
+
+    for (int run = 0; run < NUM_RUNS; ++run) {// for each run set up data structures
+        vector<string> data_vector;
+        list<string> data_list;
+        set<string> data_set;
+    }
+
+}
